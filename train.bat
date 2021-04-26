bbc462798e599d53af90129c5f4a4429560f44cb
Sebastian Buck <38660441+frunika@users.noreply.github.com>
2021-04-26 23:14:11 +0200

    added yolo_v5m_6cl_finetune

--- train.bat
+++ train.bat
@@ -2,4 +2,5 @@ call .\venv\Scripts\activate
 
 @REM yolov5m retrain coco 6cl
 cd yolov5
-python .\train.py --weights "yolov5m.pt" --cfg "../OTLabels/models/yolov5m_6cl.yaml" --data "../OTLabels/data/coco_6cl.yaml" --epochs 150 --batch-size 64 --project "OTLabels" --name "yolo_v5m_6cl"
+@REM python .\train.py --weights "yolov5m.pt" --cfg "../OTLabels/models/yolov5m_6cl.yaml" --data "../OTLabels/data/coco_6cl.yaml" --epochs 150 --batch-size 64 --project "OTLabels" --name "yolo_v5m_6cl"
+python .\train.py --weights "yolov5m.pt" --cfg "../OTLabels/models/yolov5m_6cl.yaml" --data "../OTLabels/data/coco_6cl.yaml" --hyp "data/hyp.finetune.yaml" --epochs 150 --batch-size 64 --project "OTLabels" --name "yolo_v5m_6cl_finetune"
