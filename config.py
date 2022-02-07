86649cd1b97daaf72ffcf06b516f47291ef7fe1e
Randy Seng <rseng@pm.me>
2022-02-07 17:45:13 +0100

    Set config.py to train a yolov5s model from scratch on the entire COCO dataset

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ WORKING_DIR = Path(__file__).parent
 
 # Training Parameters
 BATCH_SIZE = -1  # -1 for auto-batch
-EPOCHS = 1
+EPOCHS = 300
 RESUME_TRAINING = True
 """Path to model weights(model.pt) or `True` to fetch latest run."""
 
 # Model Configuration
-MODEL_WEIGHTS = "yolov5s.pt"
+MODEL_WEIGHTS = ""
 """
 Set to model name to use pretrained weights. To train a model from scratch set
 `MODEL_WEIGHTS = ""` and `MODEL_CFG = "path/to/model_cfg.yaml"`.
 """
-MODEL_NAME = "yolov5-s_pretrainedCOCO_COCO6cls"
-MODEL_CFG = Path(WORKING_DIR, "OTLabels/models/yolov5s_6cl.yaml")
+MODEL_NAME = "yolov5-s_COCO"
+MODEL_CFG = Path(WORKING_DIR, "yolov5/models/yolov5s.yaml")
 MODEL_HYP = Path(WORKING_DIR, "OTLabels/data/hyp.finetune.yaml")
 PROJECT_NAME = Path(WORKING_DIR, "OTLabels/data/runs")
 
@@ -26,10 +26,10 @@ FORCE_DOWNLOAD_COCO = False
 FORCE_FILTERING_LABELS = False
 USE_COCO = True
 USE_CUSTOM_DATSETS = False
-FILTER_CLASSES = True
+FILTER_CLASSES = False
 
 DATA_DIR = Path(WORKING_DIR, "OTLabels/data")
-DATA_CONFIG = Path(WORKING_DIR, "OTLabels/data/coco_6cl.yaml")
+DATA_CONFIG = Path(WORKING_DIR, "OTLabels/data/coco.yaml")
 LABELS_CVAT = Path(WORKING_DIR, "OTLabels/labels_CVAT.txt")
 
 # COCO Paths
