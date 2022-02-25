0f3c8911a7d4fc1b699de0fc005f9c4f30e979ff
Randy Seng <rseng@pm.me>
2022-02-26 00:16:33 +0100

    Change resume.py's behaviour in regards to determining the latest run in accordance to what is specified in the training config file.
    Furthermore, resuming a run is aborted if If no last.pt file
    could be found.

--- resume.py
+++ resume.py
@@ -7,53 +7,65 @@ import config as CONFIG
 from config import load_config
 
 
-def main(config_path):
+def main(config_path, project_path=None, model_name=None, last_pt_path=None):
     config = load_config(config_path)
     try:
         # Use last.pt as model weight and create another training session
-        last_pt, current_model_name = _get_last_pt_path_and_next_model_name(config)
+        if project_path is None:
+            project_name = config["project_name"]
+        else:
+            project_name = project_path
+
+        if model_name is None or last_pt_path is None:
+            (
+                last_pt_path,
+                model_name,
+            ) = _get_current_model_name_and_weight_from_latest_run(config)
+
         train.run(
-            weights=last_pt,
             cfg=config["model_cfg"],
             data=config["data_config"],
             hyp=config["model_hyp"],
             epochs=config["epochs"],
             batch_size=config["batch_size"],
-            project=config["project_name"],
-            name=current_model_name,
-            resume=True,
+            project=project_name,
+            name=model_name,
+            resume=last_pt_path,
         )
-    except FileNotFoundError:
-        # First run with settings specified in config
-        train.run(
-            weights=config["model_weights"],
-            cfg=config["model_cfg"],
-            data=config["data_config"],
-            hyp=config["model_hyp"],
-            epochs=config["epochs"],
-            batch_size=config["batch_size"],
-            project=config["project_name"],
-            name=config["model_name"],
-            resume=True,
-        )
+    except FileNotFoundError as fnfe:
+        print(fnfe)
 
 
-def _get_last_pt_path_and_next_model_name(config: dict) -> Tuple[Path, str]:
+def _get_current_model_name_and_weight_from_latest_run(
+    config: dict,
+) -> Tuple[Path, str]:
     wandb_project_dir = Path(config["project_name"])
     model_name = config["model_name"]
     if not wandb_project_dir.exists():
-        raise FileNotFoundError(f"Directory [{wandb_project_dir}]doesn't exist!")
+        raise FileNotFoundError(f"Directory [{wandb_project_dir}] doesn't exist!")
 
     current_idx = len([_dir for _dir in wandb_project_dir.iterdir() if _dir.is_dir()])
     assert current_idx != 0, f"Empty directory! [{wandb_project_dir}]"
 
     if current_idx == 1:
-        last_pt_path = wandb_project_dir / f"{model_name}/weights/last.pt"
+        latest_run_dir = wandb_project_dir / f"{model_name}"
+        current_model_name = f"{model_name}"
     else:
-        last_pt_path = wandb_project_dir / f"{model_name}_{current_idx}/weights/last.pt"
+        latest_run_dir = wandb_project_dir / f"{model_name}_{current_idx}"
+        current_model_name = f"{model_name}_{current_idx}"
 
-    current_model_name = f"{model_name}_{current_idx}"
-    assert last_pt_path.is_file(), f"File '{last_pt_path}' doesn't exist!"
+    if not latest_run_dir.exists():
+        raise FileNotFoundError(
+            f"Latest run directory could not be found. Actual path: [{latest_run_dir}]"
+        )
+    last_pt_path = Path(latest_run_dir, "weights/last.pt")
+
+    if not last_pt_path.is_file():
+        # TODO: recursively search for next available
+        raise FileNotFoundError(
+            f"last.pt file not found at: '{last_pt_path}'! "
+            + "\nNot able to resume training."
+        )
 
     return last_pt_path, current_model_name
 
