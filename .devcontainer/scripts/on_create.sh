4aa2813dd32be474ae88f3bbfd8a918c002d2a2d
Lars Briem <lars.briem@platomo.de>
2023-01-20 09:34:42 +0000

    Remove old requirements file

--- .devcontainer/scripts/on_create.sh
+++ .devcontainer/scripts/on_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install --upgrade PIP
-pip install -r requirements.txt -r requirements-dev.txt -r requirements-m1.txt
+pip install -r requirements.txt -r requirements-dev.txt
 pre-commit install --install-hooks
