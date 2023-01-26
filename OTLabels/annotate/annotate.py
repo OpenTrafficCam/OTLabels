757676d63270504b3d04edec9c9dba8206fa0d04
Michael Heilig <michael.heilig@querplaner.de>
2023-01-26 15:18:52 +0000

    Add parameters to export_data()

--- OTLabels/annotate/annotate.py
+++ OTLabels/annotate/annotate.py
@@ -10,6 +10,7 @@ class CVAT:
         self,
         url: str,
         security_file: str = "OTLabels/config/security.json",
+        organization_name: str = "OpenTrafficCam",
         project_name: str = "",
         class_file: str = "",
     ):
@@ -22,27 +23,35 @@ class CVAT:
         self.username = self.security["username"]
         self.password = self.security["password"]
         self.project_name = project_name
+        self.organization_name = organization_name
 
         if class_file != "":
             with open(class_file) as json_file:
                 self.classes = json.load(json_file)
 
-    def export_data(self, anno_key: str) -> None:
+    def export_data(
+        self,
+        anno_key: str,
+        label_field: str = "pre_annotation",
+        segment_size: int = 25,
+        task_assignees: list = ["michael"],
+        job_assignees: list = ["martin"],
+    ) -> None:
         dataset = fo.load_dataset("OTLabels")
 
         dataset.annotate(
             anno_key=anno_key,
-            label_field="pre_annotation",
+            label_field=label_field,
             classes=self.classes,
             label_type="detections",
-            segment_size=25,
-            task_assignees=["michael"],
-            job_assignees=["martin"],
+            segment_size=segment_size,
+            task_assignees=task_assignees,
+            job_assignees=job_assignees,
             username=self.username,
             password=self.password,
             url=self.url,
             project_name=self.project_name,
-            headers={"X-Organization": "OpenTrafficCam"},
+            headers={"X-Organization": self.organization_name},
         )
 
     def import_data(self, anno_key: str) -> None:
