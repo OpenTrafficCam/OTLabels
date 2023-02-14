7f1fd9548c8ff31e0e663ebfe6d8437e31ca6260
Michael Heilig <michael.heilig@querplaner.de>
2023-02-14 09:26:08 +0000

    Add testscript to preprocess data

--- OTLabels/preprocess_data.py
+++ OTLabels/preprocess_data.py
@@ -1,30 +1,41 @@
 """Preprocess image data for annotation in CVAT"""
 
-from annotate.pre_annotate import PreAnnotateImages
+# from annotate.pre_annotate import PreAnnotateImages
 
 from OTLabels.annotate.annotate import CVAT
-from OTLabels.images.import_images import ImportImages
 
-PreAnnotateImages(
-    config_file="data/image_data/training_data.json",
-    class_file="OTLabels/config/classes_COCO.json",
-    model_file="yolov8m.pt",
-).pre_annotate()
+# from OTLabels.images.import_images import ImportImages
 
-ImportImages(
-    config_file="data/image_data/training_data.json",
-    class_file="OTLabels/config/classes_COCO.json",
-).initial_import(
-    import_labels=True,
-    launch_app=True,
-    overwrite=True,
-)
+# PreAnnotateImages(
+#     config_file="data/image_data/training_data.json",
+#     class_file="OTLabels/config/classes_COCO.json",
+#     model_file="yolov8m.pt",
+# ).pre_annotate()
+
+# imp = ImportImages(
+#     config_file="data/image_data/training_data.json",
+#     class_file="OTLabels/config/classes_COCO.json",
+# )
+# imp.delete_dataset("OTLabels_no_bicycles_preannotated")
+# imp.initial_import(
+#     import_labels=True,
+#     launch_app=True,
+#     name="OTLabels",
+#     overwrite=True,
+# )
 
 cvat = CVAT(
     url="https://label.opentrafficcam.org/",
-    project_name="test_fiftyone",
+    project_name="OTLabels",
     class_file="OTLabels/config/classes_OTC.json",
 )
-
-cvat.export_data(anno_key="manual_samples")
-cvat.import_data(anno_key="manual_samples", launch_app=True)
+cvat.export_data(
+    anno_key="first_samples",
+    samples=1000,
+    segment_size=100,
+    exclude_labels=("bicyclist", "motorcyclist"),
+    job_reviewers=["martin", "michael"],
+    include_classes=("pedestrian", "truck", "bus", "motorcyclist"),
+    overwrite=True,
+)
+# cvat.import_data(anno_key="manual_samples", launch_app=True)
