f1f12edc4c1ad85b22469155fa1fcdcb3e9d0032
Michael Heilig <michael.heilig@querplaner.de>
2023-02-14 09:25:44 +0000

    Add sampling and filtering to annotate.py

--- OTLabels/annotate/annotate.py
+++ OTLabels/annotate/annotate.py
@@ -3,6 +3,7 @@
 import json
 
 import fiftyone as fo
+from fiftyone import ViewField as F
 
 
 class CVAT:
@@ -33,20 +34,49 @@ class CVAT:
         self,
         anno_key: str,
         label_field: str = "pre_annotation",
+        samples: int = 0,
+        exclude_labels: tuple = (),
         segment_size: int = 25,
-        task_assignees: list = ["michael"],
+        task_assignee: str = "michael",
         job_assignees: list = ["martin"],
+        job_reviewers: list = [],
+        dataset_name: str = "OTLabels",
+        include_classes: tuple = (),
+        overwrite: bool = False,
     ) -> None:
-        dataset = fo.load_dataset("OTLabels")
+        dataset = fo.load_dataset(dataset_name)
 
-        dataset.annotate(
+        if exclude_labels != ():
+            dataset.filter_labels(
+                "pre_annotation", eval("F('label').is_in({exclude_labels})")
+            ).tag_labels("exclude")
+            dataset.delete_labels(tags="exclude")
+            dataset_filtered = dataset
+        else:
+            dataset_filtered = dataset
+
+        if include_classes != ():
+            match = F("label").is_in(include_classes)
+            dataset_filtered = dataset_filtered.match_labels(filter=match)
+
+        if samples > 0:
+            dataset_filtered = dataset_filtered.take(samples)
+
+        if overwrite:
+            runs = dataset_filtered.list_annotation_runs()
+
+            if anno_key in runs:
+                dataset_filtered.delete_annotation_run(anno_key)
+
+        dataset_filtered.annotate(
             anno_key=anno_key,
             label_field=label_field,
             classes=self.classes,
             label_type="detections",
             segment_size=segment_size,
-            task_assignees=task_assignees,
+            task_assignee=task_assignee,
             job_assignees=job_assignees,
+            job_reviewers=job_reviewers,
             username=self.username,
             password=self.password,
             url=self.url,
@@ -54,8 +84,13 @@ class CVAT:
             headers={"X-Organization": self.organization_name},
         )
 
-    def import_data(self, anno_key: str, launch_app: bool = True) -> None:
-        dataset = fo.load_dataset("OTLabels")
+    def import_data(
+        self,
+        anno_key: str,
+        launch_app: bool = True,
+        dataset_name: str = "OTLabels",
+    ) -> None:
+        dataset = fo.load_dataset(dataset_name)
 
         dataset.load_annotations(
             anno_key=anno_key,
