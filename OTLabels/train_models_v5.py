6758b29b1d48f02cfe7a6d0aaee6890d6c11a828
Michael Heilig <michael.heilig@querplaner.de>
2023-03-29 20:45:19 +0000

    Train yolov5 models

--- OTLabels/train_models_v5.py
+++ OTLabels/train_models_v5.py
@@ -1,22 +1,8 @@
-import torch
-from ultralytics import checks
-
-from OTLabels.helpers import machine
+from yolov5 import train
 
 
 def train_basic_model():
-    checks()
-    model = torch.hub.load(
-        "ultralytics/yolov5", "yolov5s", autoshape=False
-    )  # pass any model type
-    device = get_device()
-    model.train(data="config/train-config-prototype.yaml", epochs=1, device=device)
-
-
-def get_device():
-    if machine._has_cuda():
-        return "cuda:0"
-    return "cpu"
+    train.run(data="config/train-config-prototype.yaml")
 
 
 if __name__ == "__main__":
