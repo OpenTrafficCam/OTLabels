c57337ccb63c88f842988b7f71cde5e575422c80
Lars Briem <lars.briem@platomo.de>
2024-11-27 17:04:51 +0100

    Fix loading of classifications

--- OTLabels/helpers/classification.py
+++ OTLabels/helpers/classification.py
@@ -6,5 +6,8 @@ from annotate.otc_classes import OtcClass
 
 def load_classes(class_file: str | PathLike) -> dict[OtcClass, int]:
     with open(class_file) as json_file:
-        return {OtcClass.from_string(key): value for key, value in json.load(json_file)}
+        return {
+            OtcClass.from_string(key): value
+            for key, value in json.load(json_file).items()
+        }
     return {}
