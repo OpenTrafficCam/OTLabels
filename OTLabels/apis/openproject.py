3a9171e2aebfc39ec4d43ad9752be2f7cd61d071
Lars Briem <lars.briem@platomo.de>
2024-11-28 15:02:55 +0100

    Clean up api access

--- OTLabels/apis/openproject.py
+++ OTLabels/apis/openproject.py
@@ -1,6 +1,3 @@
-from math import ceil
-from urllib.error import HTTPError
-
 import requests
 from annotate.pre_annotate import User
 
@@ -12,11 +9,6 @@ class OpenProject:
     url: str = "https://openproject.platomo.de"
     _api_route: str = "/api/v3"
     _base_url: str = url + _api_route
-    query_id: str = "1041"
-    to_be_scheduled_status_href = _api_route + "/statuses/5"
-    scheduled_status_href = _api_route + "/statuses/6"
-    in_motion_custom_field = "customField13"
-    importance_custom_field = "customField8"
     reviewer_custom_field = "customField5"
 
     def __init__(self, api_key: str) -> None:
@@ -27,93 +19,6 @@ class OpenProject:
         session = requests.Session()
         session.auth = (self._user, self._api_key)
         return session
-
-    def get_work_packages_to_schedule(self) -> list:
-        params = {}
-        work_packages = []
-        offset = 1
-        max_offset = 1
-        while offset <= max_offset:
-            params["offset"] = offset
-            query = self._session.get(
-                f"{self._base_url}/queries/{self.query_id}", params=params
-            ).json()
-            results = query["_embedded"]["results"]
-            max_offset = ceil(results["total"] / results["pageSize"])
-            work_packages.extend(results["_embedded"]["elements"])
-            offset += 1
-
-        return work_packages
-
-    def get_work_package_link(self, work_package: dict) -> str:
-        url = self.url + work_package["_links"]["self"]["href"].removeprefix(
-            self._api_route
-        )
-        return url
-
-    @staticmethod
-    def get_work_package_type(work_package: dict) -> str:
-        wp_type = work_package["_links"]["type"]["title"]
-        return wp_type
-
-    def get_work_package_priority(self, work_package: dict) -> int:
-        # 7: Low
-        # 8: Normal
-        # 9: High
-        # 10: Immediate
-        wp_priority = work_package["_links"]["priority"]["href"].removeprefix(
-            self._api_route + "/priorities/"
-        )
-        return int(wp_priority)
-
-    def get_work_package_importance(self, work_package: dict) -> int:
-        # 8: High
-        # 9: Normal
-        # 10: Low
-
-        wp_priority = work_package["_links"][self.importance_custom_field][
-            "href"
-        ].removeprefix(self._api_route + "/custom_options/")
-        return int(wp_priority)
-
-    def set_in_motion(self, work_package: dict) -> None:
-
-        wp_id = work_package["id"]
-        lock_version = work_package["lockVersion"]
-        current_status = work_package["_links"]["status"]["href"]
-        body = {
-            self.in_motion_custom_field: True,
-            "lockVersion": lock_version,
-        }
-        if current_status == self.to_be_scheduled_status_href:
-            body["_links"] = {"status": {"href": self.scheduled_status_href}}
-        response = self._session.patch(
-            f"{self._base_url}/work_packages/{wp_id}",
-            json=body,
-            params={"notify": False},
-        )
-        if response.status_code != 200:
-            raise HTTPError(
-                response.url,
-                response.status_code,
-                response.text,
-                response.headers,  # type: ignore
-                response.content,  # type: ignore
-            )
-        comment = {"comment": {"raw": "Automatically added to motion."}}
-        comment_response = self._session.post(
-            f"{self._base_url}/work_packages/{wp_id}/activities",
-            json=comment,
-            params={"notify": False},
-        )
-        if comment_response.status_code != 201:
-            raise HTTPError(
-                response.url,
-                response.status_code,
-                response.text,
-                response.headers,  # type: ignore
-                response.content,  # type: ignore
-            )
 
     def create_task(
         self, title: str, assignee: User, reviewer: User, content: str, project_id: int
